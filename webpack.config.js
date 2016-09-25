ab43cbcfaaad0a7f61cb9d4f4cd519ae09e34552
chenag <23110134@qq.com>
2016-09-26 01:10:08 +0800

    modify config

--- webpack.config.js
+++ webpack.config.js
@@ -32,18 +32,22 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var config = {
     // 开发调试信息
     devtool: "source-map",
+    // 文件入口
     entry: getEntry(),
     //entry:'./src/js/page/Person_use.js',
+    // 输出文件配置
     output: {
         path: path.join(__dirname, "build" + jspage), //文件输出目录
         // publicPath: 'http://localhost:63342/webpack_self_demo160914/build/',
         filename: "[name].js",
         sourceMapFilename: "[file].map"
     },
-    // resolve: {
-    //     alias: {},
-    //     extensions: ['', '.js', '.jsx']
-    // },
+    // 文件后缀名自动补全
+    //resolve: {
+    //    alias: {},
+    //    extensions: ['', '.js', '.jsx']
+    //},
+    // 模块配置，处理文件的loader配置
     module: {
         loaders: [
             // 样式被打包到脚本中
@@ -51,11 +55,13 @@ var config = {
             /* 样式不被打包到脚本中，而是独立出来作为.css，
              * 然后在页面中以<link>标签引入
              */
+            // 可与gulp配合使用，页面中引用gulp处理好的css
             {test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader")},
             {test: /\.scss$/, loader: "style!css!sass"},
             {test: /\.(png|jpg)$/, loader: 'url'},
             {
                 //test: path.join(__dirname, 'js/page'),
+                //test: /\.js$/,
                 test: /\.js|jsx$/,
                 exclude: "/node_modules/",
                 loader: 'babel-loader',
@@ -63,25 +69,32 @@ var config = {
                     presets: ['es2015']
                 }
             }
+            //,
             //{
-            //    test: /\.js?$/,
+            //    test: /\.jsx$/,
+            //    exclude: "/node_modules/",
+            // 还未安装react-hot
             //    loaders: ['react-hot', 'babel'],
-            //    exclude: /node_modules/
+            //    query: {
+            //        presets: ['es2015']
+            //        //presets: ['es2015','react']
+            //    }
             //}
         ]
     },
+    // 插件配置
     plugins: [
         //new IgnorePlugin(/jquery/),
-        //此处开启，html页面必须引入公用js
+        // 此处开启，html页面必须引入公用js
         //new CommonsChunkPlugin({
         //    filename: "common.js",
         //    name: "common"
         //}),
-        // new UglifyJsPlugin({
-        //     compress: {
-        //         warnings: false
-        //     }
-        // }),
+        new UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
         new ExtractTextPlugin("[name].css")
     ]
 };
