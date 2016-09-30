34eb4967ef5d734268b2d2d1e886961f5715edf0
chenanguo <chenanguo@le.com>
2016-09-30 11:29:54 +0800

    add

--- src/js/page/import_var.js
+++ src/js/page/import_var.js
@@ -1,6 +1,8 @@
 /**
  * Created by chenanguo on 2016/9/30.
  */
-import {name,age} from "./export_var.js";
-console.log("name = "+ name);
-console.log("age = "+ age);
+// webpack编译es6 module报错，尚不知原因。export文件后缀修改为.jsx,就能编译通过
+// import {name,age} from "./export_var.js";
+import {name, age} from "./export_var.jsx";
+console.log("name = " + name);
+console.log("age = " + age);
